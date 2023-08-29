da58173ec37438717542937ec8a6c6ee99add09c
Heitor Ramos <heitorr.fonseca@gmail.com>
2023-08-29 16:50:13 -0300

    Criação do botão e função update

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("./Category");
-const Slugify = require("slugify")
+const Slugify = require("slugify");
+const { default: slugify } = require("slugify");
 
 router.get("/admin/categories/new",(req,res)=>{
     res.render("admin/categories/new")
@@ -14,7 +15,7 @@ router.post("/categories/save",(req,res) => {
             title:title,
             slug:Slugify(title)
         }).then(()=>{
-            res.redirect("/");
+            res.redirect("/admin/categories");
         })
     }else{
         res.redirect("/admin/categories/new");
@@ -52,8 +53,39 @@ router.post("/categories/delete", (req,res) => {
     }
 });
 
-router.post("/categories/update", (req,res){
+router.get("/admin/categories/edit/:id", (req,res) => {
+    let id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category =>{
+        if(category != undefined){
+            
+            res.render("admin/categories/edit",{category: category});
+
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
     
+});
+
+router.post("/categories/update",(req,res) => {
+    let id = req.body.id;
+    let title  = req.body.title;
+
+    Category.update({title:title, slug:slugify(title)},{
+        where:{
+            id: id
+        }
+    }).then(() =>{
+        res.redirect("/admin/categories");
+    })
 })
 
 module.exports = router;
+
